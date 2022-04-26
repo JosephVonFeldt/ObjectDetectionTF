f54997c4b50947fea9b6175e7e5e042fff11a4ae
JoeyV <joey.vonfeldt@gmail.com>
2022-04-25 21:04:36 -0700

    create_splits implementation and EDA update

--- create_splits.py
+++ create_splits.py
@@ -2,10 +2,11 @@ import argparse
 import glob
 import os
 import random
-
+import shutil
 import numpy as np
-
-from utils import get_module_logger
+from os import listdir
+from os.path import isfile, join
+#from utils import get_module_logger
 
 
 def split(source, destination):
@@ -17,10 +18,22 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
-
+    fnames = [f for f in listdir(source) if isfile(join(source, f)) and len(f)>9 and f[-9:] == '.tfrecord']
+    destinationFiles = [destination+'/'+f for f in fnames]
+    random.shuffle(fnames)
+    train_num = (len(fnames)*17)//20
+    val_num = len(fnames)//10
+    for i,f in enumerate(fnames):
+        if i< train_num:
+            os.rename(source + '/' + f, destination + '/train/' + f)
+        elif i< train_num + val_num:
+            os.rename(source + '/' + f, destination + '/val/' + f)
+        else:
+            os.rename(source + '/' + f, destination + '/test/' + f)
 
 if __name__ == "__main__":
+    #split('data/processed', 'data')
+
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
     parser.add_argument('--source', required=True,
                         help='source data directory')
